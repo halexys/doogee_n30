a13d50250d18ea130725197c8c680a15eab4b7f5
halexys <72656657+halexys@users.noreply.github.com>
2023-05-21 16:11:30 -0400

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_N30.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_N30-user \
+    twrp_N30-userdebug \
+    twrp_N30-eng
